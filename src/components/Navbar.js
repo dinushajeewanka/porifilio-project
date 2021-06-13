0ec197b58f6b11ad1c2aeab6afc4f86ae5024c72
Dinusha Jeewanka <58622301+dinushajeewanka@users.noreply.github.com>
2021-06-13 19:23:19 +0530

    m

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,29 @@ import logo from '../logo.jpg';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-< div className="container">
-  <a class="navbar-brand" href="#/"><img className ="logo" src={logo} alt="logo..."/></a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+  < div className="container">
+    <a class="navbar-brand" href="#/"><img className ="logo" src={logo} alt="logo..."/></a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav ml-auto">
-      <li className="nav-item active">
-        <a className="nav-link ml-auto" href="#/">Home<span className="sr-only"></span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link ml-auto" href="#/">Link</a>
-      </li>
-    </ul>
-  </div>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <a className="nav-link ml-auto" href="#/">Home<span className="sr-only"></span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link ml-auto" href="#/">Education</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link ml-auto" href="#/">Experince</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link ml-auto" href="#/">Link</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
     )
