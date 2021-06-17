ea162cf80e43342c20f6b1115381de64523a9280
Dinusha Jeewanka <58622301+dinushajeewanka@users.noreply.github.com>
2021-06-17 13:27:01 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import logo from '../logo.jpg';
 
+// REACT FONTAWESOME IMPORTS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 const Navbar = () => {
     return (
-<nav className="navbar navbar-expand-lg navbar-light bg-dark">
-  < div className="container">
-    <a class="navbar-brand" href="#/"><img className ="logo" src={logo} alt="logo..."/></a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+  <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    < div className="container">
+      <a class="navbar-brand" href="#/"><img className ="logo" src={logo} alt="logo..."/></a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }}/>
+      </button>
 
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item active">
-          <a className="nav-link ml-auto" href="#/">Home<span className="sr-only"></span></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link ml-auto" href="#/">Education</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link ml-auto" href="#/">Experince</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link ml-auto" href="#/">Link</a>
-        </li>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <a className="nav-link ml-auto" href="#/">Home<span className="sr-only"></span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link ml-auto" href="#/">About-Me</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link ml-auto" href="#/">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link ml-auto" href="#/">How Work</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link ml-auto" href="#/">Portfolio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link ml-auto" href="#/">Contacts</a>
+          </li>
       </ul>
     </div>
   </div>
-</nav>
+  </nav>
     )
 }
 
